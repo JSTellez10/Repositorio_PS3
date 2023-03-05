547f46c41707a08bbd9b63a93531bddd64286225
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-05 14:49:22 -0500

    Create 4_Clusters.R

---
+++ scripts/4_Clusters.R
@@ -0,0 +1,12 @@
+
+#------------------------------------------------------------------------------#
+#
+#                                4 - CLUSTERS
+#
+#------------------------------------------------------------------------------#
+ 
+ #Regresión----
+ 
+ reg1 <- lm(price~distancia_hospital + area_hospital, data = train)
+ stargazer(reg1,type = "text", dep.var.labels = "Precio del Inmueble", digits = 0)
+ 
