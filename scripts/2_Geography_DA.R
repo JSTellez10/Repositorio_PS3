7f4b39a099ca9ffda32348b37f66c3f39cf9732c
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-07 16:04:44 -0500

    Update 2_Geography_DA.R

--- scripts/2_Geography_DA.R
+++ scripts/2_Geography_DA.R
@@ -1,11 +1,13 @@
 
-  rm(list=ls())
-  
-  require("pacman")
-  p_load(tidyverse, sf, tmaptools, osmdata, leaflet) 
+#------------------------------------------------------------------------------#
+#
+#                        2 - GEOGRAPHICAL ENVIRONMENT
+#
+#------------------------------------------------------------------------------#
 
-  
 #Leemos los datos geográficos
+
+  #Datos descargados de la página Datos Abiertos de Bogotá - 6 e marzo de 2023
 
   localidades <-st_read("datos/localidades")
   parques <-st_read("datos/parques")
@@ -14,7 +16,12 @@
   ese <-st_read("datos/ese")
   colegios <-st_read("datos/colegios")
   cai <-st_read("datos/cai")
+  biblioestacion <-st_read("datos/bibloestacion")
+  centro_financiero <-st_read("datos/centro_financiero")
 
+
+  #Datos descargados de Open Street Map
+  
   chapinero <- getbb(place_name = "UPZ Chapinero, Bogotá",
                      featuretype = "boundary:administrative",
                      format_out = "sf_polygon") %>% .$multipolygon
@@ -25,36 +32,45 @@
   
   bogota <- st_transform(bogota, 4686)
 
+  bogota_mapa <- opq(bbox = getbb("Bogotá Colombia"))
+  
 #Transformamos los archivos de cada variable
   
-  st_crs(parques)
-  head(parques)
+  #Localidades
+  localidades <- st_transform(localidades,4686)
+  sf_localidades = localidades %>% st_sf
+  
+  #Parques
   parques <- st_transform(parques,4686)
-  sf_parques = parques %>% st_sf %>% st_cast 
-  head(sf_parques)
-
-
-  sf_parques<-st_as_sf(parques,coords=c('longp','latp'),crs=4326)
+  sf_parques = parques %>% st_sf
   
-#Aseguramos que todo se encuentre en los límites de Bogotá
+  #Museos
+  museos <- st_transform(museos,4686)
+  sf_museos = museos %>% st_sf
   
-  sf_parques2 <- gIntersects(sf_localidades$geometry , sf_parques$geometry)
-  sf_parques2 <- st_overlaps(sf_parques,bogota)
+  #IPS
+  ips <- st_transform(ips,4686)
+  sf_ips = ips %>% st_sf
   
+  #ESE
+  ese <- st_transform(ese,4686)
+  sf_ese = ese %>% st_sf
   
-  limites <- getbb("Bogota Colombia") #Aseguramos que sean inmuebles en Bogotá
+  #Colegios
+  colegios <- st_transform(colegios,4686)
+  sf_colegios = colegios %>% st_sf
   
-  filtro1 <- between(sf_parques$long, limites[1, "min"], limites[1, "max"])
-  filtro2 <- between(sf_parques$lat, limites[2, "min"], limites[2, "max"])
-  filtro <- filtro1 & filtro2
+  #CAI
+  cai <- st_transform(cai,4686)
+  sf_cai = cai %>% st_sf
   
-  sf_parques <- sf_parques[filtro,]
-  sf_museos <- sf_museos[filtro,]
-  sf_ips <- sf_ips[filtro,]
-  sf_ese <- sf_ese[filtro,]
-  sf_colegios <- sf_colegios[filtro,]
-  sf_cai <- sf_cai[filtro,]
-  sf_localidades <- sf_localidades[filtro,]
+  #Biblioestacion
+  biblioestacion <- st_transform(biblioestacion,4686)
+  sf_biblioestacion = biblioestacion %>% st_sf
+  
+  #Centro financiero
+  centro_financiero <- st_transform(centro_financiero,4686)
+  sf_centro_financiero = centro_financiero %>% st_sf
   
 #Generamos los centroides de cada variable
   
@@ -64,13 +80,8 @@
   centroides_ese      <- gCentroid(as(sf_ese$geometry, "Spatial"), byid = T)
   centroides_colegios <- gCentroid(as(sf_colegios$geometry, "Spatial"), byid = T)
   centroides_cai      <- gCentroid(as(sf_cai$geometry, "Spatial"), byid = T)
-  
-  #localidades2 <-st_as_sf(localidades,coords=c('longp','latp'),crs=4326)
-  #parques2     <-st_as_sf(parques,coords=c('longp','latp'),crs=4326)
-  #museos2      <-st_as_sf(museos,coords=c('longp','latp'),crs=4326)
-  #ips2         <-st_as_sf(ips,coords=c('longp','latp'),crs=4326)
-  #ese2         <-st_as_sf(ese,coords=c('longp','latp'),crs=4326)
-  #colegios2    <-st_as_sf(colegios,coords=c('longp','latp'),crs=4326)
+  centroides_best     <- gCentroid(as(sf_biblioestacion$geometry, "Spatial"), byid = T)
+  centroides_centrof  <- gCentroid(as(sf_centro_financiero$geometry, "Spatial"), byid = T)
 
   
 #Mapa Completo
@@ -80,12 +91,14 @@
     addPolygons(data=bogota, color = "#5CACEE", opacity = 0.4) %>%
     addCircles(data=train, radius = 0.1, color = "#DAA520", opacity = 2) %>%
     addPolygons(data=chapinero, color = "#008B45", opacity = 0.4)%>%
-    #addPolygons(data=sf_localidades, color = "#5CACEE", opacity = 1)%>%
+    addPolygons(data=sf_localidades, color = "#5CACEE", opacity = 1)%>%
     addCircles(lng = centroides_colegios$x, lat = centroides_colegios$y, col = "#00CDCD", opacity = 0.8, radius = 0.5, popup = sf_colegios$NOMBRE_EST)%>%
     addCircles(lng = centroides_parques$x, lat = centroides_parques$y, col = "#00EE00", opacity = 0.8, radius = 0.5, popup = sf_parques$NOMBRE)%>%
     addCircles(lng = centroides_ips$x, lat = centroides_ips$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ips$nombre_pre)%>%
     addCircles(lng = centroides_ese$x, lat = centroides_ese$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ese$RSOENTADSC)%>%
     addCircles(lng = centroides_museos$x, lat = centroides_museos$y, col = "#0000FF", opacity = 0.8, radius = 0.5, popup = sf_museos$LecNombre)%>%
-    addCircles(lng = centroides_cai$x, lat = centroides_cai$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_cai$CAIDESCRIP)
-
+    addCircles(lng = centroides_cai$x, lat = centroides_cai$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_cai$CAIDESCRIP) %>%
+    addCircles(lng = centroides_best$x, lat = centroides_best$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_biblioestacion$LecNombre) %>%
+    addCircles(lng = centroides_centrof$x, lat = centroides_centrof$y, col = "#008B00", opacity = 0.8, radius = 0.5)  #%>%
+  
   
