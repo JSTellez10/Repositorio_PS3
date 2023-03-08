59766b1b857dbbd4898ea649f02915b125737909
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-03-08 14:07:50 -0500

    Update 2_Geography_DA.R

--- scripts/2_Geography_DA.R
+++ scripts/2_Geography_DA.R
@@ -82,6 +82,25 @@
   centro_cuadrantepolicia <- st_transform(cuadrantepolicia,4686)
   sf_centro_cuadrantepolicia = centro_cuadrantepolicia %>% st_sf
   
+  #Paradas de buses----
+  
+  buses <- bogota %>% add_osm_feature(key="highway",value="bus_stop") %>% osmdata_sf()
+  buses_puntos <- buses$osm_point
+  
+  leaflet() %>%
+    addTiles() %>%
+    addCircles(data=buses_puntos, color = "#27408B", opacity = 0.7)
+  
+  #Estaciones de Transmilenio----
+  
+  estaciones_tm <- bogota %>% add_osm_feature(key="amenity", value = "bus_station") %>% osmdata_sf()
+  estaciones_tm_puntos <- estaciones_tm$osm_points
+  
+  leaflet() %>%
+    addTiles() %>%
+    addCircles(data=estaciones_tm_puntos, color = "#00868B", opacity = 1)
+  
+  
 #Generamos los centroides de cada variable
   
   centroides_parques  <- gCentroid(as(sf_parques$geometry, "Spatial"), byid = T)
@@ -92,7 +111,7 @@
   centroides_cai      <- gCentroid(as(sf_cai$geometry, "Spatial"), byid = T)
   centroides_best     <- gCentroid(as(sf_biblioestacion$geometry, "Spatial"), byid = T)
   centroides_centrof  <- gCentroid(as(sf_centro_financiero$geometry, "Spatial"), byid = T)
-  centroides_delitos  <- gCentroid(as(sf_centro_delitos$geometry, "Spatial"), byid = T)
+  centroides_delitos  <- gCentroid(as(delitos_geom$geometry, "Spatial"), byid = T)
   centroides_cuadrantes  <- gCentroid(as(sf_centro_cuadrantepolicia$geometry, "Spatial"), byid = T)
   
   
@@ -100,17 +119,19 @@
 
   leaflet() %>%
     addTiles() %>%
-    addPolygons(data=bogota, color = "#5CACEE", opacity = 0.4) %>%
-    addCircles(data=train, radius = 0.1, color = "#DAA520", opacity = 2) %>%
+    #addPolygons(data=bogota, color = "#5CACEE", opacity = 0.4) %>%
+    #addCircles(data=train, radius = 0.1, color = "#DAA520", opacity = 2) %>%
     addPolygons(data=chapinero, color = "#008B45", opacity = 0.4)%>%
-    addPolygons(data=sf_localidades, color = "#5CACEE", opacity = 1)%>%
-    addCircles(lng = centroides_colegios$x, lat = centroides_colegios$y, col = "#00CDCD", opacity = 0.8, radius = 0.5, popup = sf_colegios$NOMBRE_EST)%>%
-    addCircles(lng = centroides_parques$x, lat = centroides_parques$y, col = "#00EE00", opacity = 0.8, radius = 0.5, popup = sf_parques$NOMBRE)%>%
-    addCircles(lng = centroides_ips$x, lat = centroides_ips$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ips$nombre_pre)%>%
-    addCircles(lng = centroides_ese$x, lat = centroides_ese$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ese$RSOENTADSC)%>%
-    addCircles(lng = centroides_museos$x, lat = centroides_museos$y, col = "#0000FF", opacity = 0.8, radius = 0.5, popup = sf_museos$LecNombre)%>%
-    addCircles(lng = centroides_cai$x, lat = centroides_cai$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_cai$CAIDESCRIP) %>%
-    addCircles(lng = centroides_best$x, lat = centroides_best$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_biblioestacion$LecNombre) %>%
-    addCircles(lng = centroides_centrof$x, lat = centroides_centrof$y, col = "#008B00", opacity = 0.8, radius = 0.5)  #%>%
+    #addPolygons(data=sf_localidades, color = "#5CACEE", opacity = 1)%>%
+    #addCircles(lng = centroides_colegios$x, lat = centroides_colegios$y, col = "#00CDCD", opacity = 0.8, radius = 0.5, popup = sf_colegios$NOMBRE_EST)%>%
+    #addCircles(lng = centroides_parques$x, lat = centroides_parques$y, col = "#00EE00", opacity = 0.8, radius = 0.5, popup = sf_parques$NOMBRE)%>%
+    #addCircles(lng = centroides_ips$x, lat = centroides_ips$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ips$nombre_pre)%>%
+    #addCircles(lng = centroides_ese$x, lat = centroides_ese$y, col = "#FF3030", opacity = 0.8, radius = 0.5, popup = sf_ese$RSOENTADSC)%>%
+    #addCircles(lng = centroides_museos$x, lat = centroides_museos$y, col = "#0000FF", opacity = 0.8, radius = 0.5, popup = sf_museos$LecNombre)%>%
+    #addCircles(lng = centroides_cai$x, lat = centroides_cai$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_cai$CAIDESCRIP) %>%
+    #addCircles(lng = centroides_best$x, lat = centroides_best$y, col = "#008B00", opacity = 0.8, radius = 0.5, popup = sf_biblioestacion$LecNombre) %>%
+    #addCircles(lng = centroides_centrof$x, lat = centroides_centrof$y, col = "#008B00", opacity = 0.8, radius = 0.5)  #%>%
+    #addCircles(lng = centroides_delitos$x, lat = centroides_delitos$y, col = "#008B00", opacity = 0.8, radius = 0.5) %>%
+    #addCircles(lng = centroides_cuadrantes$x, lat = centroides_cuadrantes$y, col = "#008B00", opacity = 0.8, radius = 0.5)
   
   
